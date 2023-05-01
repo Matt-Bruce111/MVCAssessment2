4aad0c14c8cea4e5658f39c05ce69a4f719d0565
Matt-Bruce111 <Matt.bruce111@gmail.com>
2023-05-01 11:33:53 +1000

    Added EntityFrameworkIdentity and created UserTables in DB

---
+++ MVCAssessment2/MVCAssessment2/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace MVCAssessment2.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> userManager { get; }
+        private SignInManager<IdentityUser> signInManager { get; }
+
+        // Dependancy Injection
+        public AccountController(UserManager<IdentityUser> _userManager,
+            SignInManager<IdentityUser> _signInManager)
+        {
+            this.userManager = _userManager;
+            this.signInManager = _signInManager;
+        }
+    }
+}
