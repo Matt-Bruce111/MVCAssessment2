c3c0d15c983476984c11c7be5f8015d16dab52d1
wkasthuri <115693081+wkasthuri@users.noreply.github.com>
2023-05-07 23:52:10 +1000

    Add files via upload

--- MVCAssessment2/MVCAssessment2/Program.cs
+++ MVCAssessment2/MVCAssessment2/Program.cs
@@ -1,7 +1,34 @@
+using MVCAssessment2.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuration Root
+IConfigurationRoot configuration; //normal variable
+configuration = new ConfigurationBuilder().AddJsonFile("./config.json").Build();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add the connection string
+builder.Services.AddDbContext<CSIROContext>(options =>
+//builder.Services.AddDbContext<ApplicantDataContext>(options =>
+{
+    var connectionString = configuration.GetConnectionString("DBConnection");
+    options.UseSqlServer(connectionString);
+});
+
+// Create a user
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<CSIROContext>()
+    //.AddEntityFrameworkStores<ApplicantDataContext>()
+    .AddDefaultTokenProviders();
+
+//APIs for registering session management
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -12,16 +39,31 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+
+app.UseSession();
 
 app.UseRouting();
 
-app.UseAuthorization();
+app.UseAuthentication();
+
+//app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    //pattern: "{controller=Home}/{action=Index}/{id?}");
+    //pattern: "{controller=Applicant}/{action=Display}/{id?}");
+    pattern: "{controller=Account}/{action=Register}/{id?}");
+    //pattern: "{controller=Account}/{action=Login}/{id?}");
+    //pattern: "{controller=Admin}/{action=CreateRole}/{id?}");
+    //pattern: "{controller=Admin}/{action=ManageRole}/{id?}");
 
 app.Run();
+
+// Package test
