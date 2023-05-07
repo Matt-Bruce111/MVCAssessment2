9645fb41cd2d708392c45e55f2d4fc56a0596324
wkasthuri <115693081+wkasthuri@users.noreply.github.com>
2023-05-07 23:45:48 +1000

    Add files via upload

---
+++ MVCAssessment2/MVCAssessment2/Controllers/ApplicantController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCAssessment2.Controllers
+{
+    public class ApplicantController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
