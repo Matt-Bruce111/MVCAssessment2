979eb3c00806de52b4a1f38d399ef63dbda1c87c
wkasthuri <115693081+wkasthuri@users.noreply.github.com>
2023-05-08 10:21:09 +1000

    Add files via upload
    view folder

---
+++ MVCAssessment2/MVCAssessment2/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCAssessment2.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Enter email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
